5b95483152dd80ccd776c29a5339f2768dd4cf45
ryabina <ryabina.scout@gmail.com>
2020-02-03 18:02:03 +0300

    commenting tests

--- src/components/atoms/Timer.test.js
+++ src/components/atoms/Timer.test.js
@@ -1,28 +1,26 @@
-/*
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import Timer from './Timer';
-import genarateRandomTime from "../../helpers/timerHelper";
-
-jest.mock('../../helpers/timerHelper');
-
-describe('GamePage', () => {
-  beforeEach(() => {
-    jest.resetModules();
-  });
-
-  test('should call timeout', () => {
-    // Arrange
-    const expectedTime = 5;
-    const mockGenerateTime = jest.fn(() => expectedTime);
-    genarateRandomTime.mockImplementation(mockGenerateTime);
-
-    const mockSetTimeout = jest.fn(() =>{}, expectedTime);
-    setTimeout.mockImplementation(mockSetTimeout);
-    // Act
-
-    // Assert
-    expect(setTimeout).toHaveBeenCalledWith(expectedTime);
-  });
-});
-*/
+// import React from 'react';
+// import '@testing-library/jest-dom/extend-expect';
+// import Timer from './Timer';
+// import genarateRandomTime from "../../helpers/timerHelper";
+//
+// jest.mock('../../helpers/timerHelper');
+//
+// describe('GamePage', () => {
+//   beforeEach(() => {
+//     jest.resetModules();
+//   });
+//
+//   test('should call timeout', () => {
+//     // Arrange
+//     const expectedTime = 5;
+//     const mockGenerateTime = jest.fn(() => expectedTime);
+//     genarateRandomTime.mockImplementation(mockGenerateTime);
+//
+//     const mockSetTimeout = jest.fn(() =>{}, expectedTime);
+//     setTimeout.mockImplementation(mockSetTimeout);
+//     // Act
+//
+//     // Assert
+//     expect(setTimeout).toHaveBeenCalledWith(expectedTime);
+//   });
+// });
